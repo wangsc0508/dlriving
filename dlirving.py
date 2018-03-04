fd499b4c531340c1475d317d047b9e301ab8f4ea
王碩志 <wangsc@wangshuozhide-MacBook-Pro.local>
2018-03-05 01:40:22 +0800

    add_usa

--- dlirving.py
+++ dlirving.py
@@ -1,13 +1,14 @@
 country = input('請問你是哪國人：')
 age = input('請問你年齡幾歲：')
 age = int(age)
-if country == '台灣'：
+if country == '台灣':
 	if age >= 18:
 		print('你可以考駕照')
 	else:
 		print('你還不能考駕照')
-if country == '美國'：
-	if age >= 16:
-		print('你可以考駕照')
+elif country == '美國':
+	if age >=16:
+		print('你可以考駕照喔')
 	else:
 		print('你還不能考駕照')
+print('您可能國家輸入錯誤喔！')
